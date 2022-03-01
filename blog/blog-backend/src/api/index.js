cab8491a45895ea5b00cd0b84cb69c2e9807d3fa
leeuihyun <meta212121@naver.com>
2022-03-01 17:48:00 +0900

    🌟React postman use

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,6 +1,10 @@
 const Router = require('koa-router');
 const api = new Router();
 
+const posts = require('./posts');
+
+api.use('/posts', posts.routes());
+
 api.get('/test', (ctx) => {
   ctx.body = 'test';
 });
--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
@@ -7,6 +8,7 @@ const router = new Router();
 const api = require('./api');
 
 router.use('/api', api.routes());
+app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, () => {
   console.log('Listening to port 4000');
