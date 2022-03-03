f09bd251a41e43ea32f86a1d10c80b79c7a5f6d2
leeuihyun <meta212121@naver.com>
2022-03-03 22:44:53 +0900

    React query(location),params(match)

--- urlpractice/src/components/About.js
+++ urlpractice/src/components/About.js
@@ -5,11 +5,17 @@ function About({ location }) {
     const query = qs.parse(location.search, {
         ignoreQueryPrefix: true, //이 설정으로 문자열 맨앞의 ?를 생략한다.
     });
-    const showDetail = query.detail === "true";
+
+    //const showDetail = query.detail === "true";
     return (
         <div>
             <h2>소개</h2>
-            {showDetail && <p>detail값을 잘 지정하셨군요!</p>}
+
+            {query.detail === "true" ? (
+                <p>detail값을 잘 지정하셨군요!</p>
+            ) : (
+                <p>detail값을 true로 지정해보세요.</p>
+            )}
         </div>
     );
 }
