d1072128dc72e4e60bf52de69109fbe3b51d74b1
leeuihyun <meta212121@naver.com>
2022-03-08 00:06:36 +0900

    App no-redux only use useState

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
+import { useState, useCallback } from 'react';
 
 function App() {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => {
+    setCategory(category);
+  }, []);
   return (
     <>
       <Categories />
