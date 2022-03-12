5abd8d943f27d80506c0b51bf3a3518e9e01fc13
leeuihyun <meta212121@naver.com>
2022-03-12 22:44:01 +0900

    React user module use redux-saga

--- blog/blog-frontend/src/modules/user.js
+++ blog/blog-frontend/src/modules/user.js
@@ -23,3 +23,23 @@ const initialState = {
   user: null,
   checkError: null,
 };
+
+export default handleActions(
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: error,
+    }),
+  },
+  initialState,
+);
