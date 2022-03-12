8dd2521f37e91ac37adc0de67d193d127c9a5547
leeuihyun <meta212121@naver.com>
2022-03-12 22:35:19 +0900

    React user redux module

---
+++ blog/blog-frontend/src/modules/user.js
@@ -0,0 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; //새로 고침 이후 임시 로그인 처리
+//회원 정보 확인
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK');
+
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
+export const check = createAction(CHECK);
+
+const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+export function* userSaga() {
+  yield takeLatest(CHECK, checkSaga);
+}
+
+const initialState = {
+  user: null,
+  checkError: null,
+};
