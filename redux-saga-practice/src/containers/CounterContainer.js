6bc467e62bd467e2a50fb45873083d04298401d9
leeuihyun <meta212121@naver.com>
2022-03-14 01:10:52 +0900

    Redux redux-saga using 1

--- redux-saga-practice/src/containers/CounterContainer.js
+++ redux-saga-practice/src/containers/CounterContainer.js
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
 import React, { useCallback } from "react";
 import { increase, decrease } from "../modules/counter";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
 import Counter from "../components/Counter";
 
 function CounterContainer() {
     const number = useSelector(({ counter }) => counter.number);
     const dispatch = useDispatch();
 
-    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
-    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    const onIncrease = useCallback(() => dispatch(increaseAsync()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decreaseAsync()), [dispatch]);
 
     return (
         <Counter
