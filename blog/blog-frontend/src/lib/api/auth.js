a653287b05b6433cbaebe9d9d31b2b9e6088ed2a
leeuihyun <meta212121@naver.com>
2022-03-15 01:12:08 +0900

    ResponsiveBlock (common component)

--- blog/blog-frontend/src/lib/api/auth.js
+++ blog/blog-frontend/src/lib/api/auth.js
@@ -5,7 +5,7 @@ export const login = ({ username, password }) =>
   client.post('/api/auth/login', { username, password });
 
 //register
-export const register = ({ username, password, passwordConfirm }) =>
-  client.post('/api/auth/register', { username, password, passwordConfirm });
+export const register = ({ username, password }) =>
+  client.post('/api/auth/register', { username, password });
 
 export const check = () => client.get('/api/auth/check');
