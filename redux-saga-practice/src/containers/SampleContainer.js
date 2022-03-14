c5ace2feec5dea712f21a44bc15db1229b59d64c
leeuihyun <meta212121@naver.com>
2022-03-14 14:48:32 +0900

    redux-saga sampleContainer

--- redux-saga-practice/src/containers/SampleContainer.js
+++ redux-saga-practice/src/containers/SampleContainer.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Sample from "../components/Sample";
 import { getPost, getUsers } from "../modules/sample";
+import { useSelector, useDispatch } from "react-redux";
 function SampleContainer() {
+    const { post, users, loadingPost, loadingUsers } = useSelector(
+        ({ sample }) => ({
+            post: sample.post,
+            users: sample.users,
+        })
+    );
     return <Sample loadingPost={loadingPost}></Sample>;
 }
 
