2cae5d2cd4f511b274cbdba6035594abc8170479
leeuihyun <meta212121@naver.com>
2022-03-15 15:56:59 +0900

    Header component

--- blog/blog-frontend/src/components/common/Header.js
+++ blog/blog-frontend/src/components/common/Header.js
@@ -9,7 +9,10 @@ const HeaderBlock = styled.div`
   background: white;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
 `;
-
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
 const Wrapper = styled(Responsive)`
   height: 4rem;
   display: flex;
@@ -30,7 +33,7 @@ const Spacer = styled.div`
   height: 4rem;
 `;
 
-function Header() {
+function Header({ user }) {
   return (
     <>
       <HeaderBlock>
@@ -38,9 +41,16 @@ function Header() {
           <Link to="/" className="logo">
             CARPE DIEM
           </Link>
-          <div className="right">
-            <Button to="login">로그인</Button>
-          </div>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button to="logout">로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="login">로그인</Button>
+            </div>
+          )}
         </Wrapper>
       </HeaderBlock>
       <Spacer />
