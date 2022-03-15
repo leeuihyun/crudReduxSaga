5d367341d5c286eac9be9467396d8a9a7487d2f7
leeuihyun <meta212121@naver.com>
2022-03-15 17:21:41 +0900

    create logout Action, logoutSaga

--- blog/blog-frontend/src/modules/user.js
+++ blog/blog-frontend/src/modules/user.js
@@ -4,19 +4,40 @@ import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
 } from '../lib/createRequestSaga';
+import { call } from 'redux-saga/effects';
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; //새로 고침 이후 임시 로그인 처리
 //회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
   createRequestActionTypes('user/CHECK');
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout);
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
+  }
+}
 
+function checkFailureSaga() {
+  //yield를 사용하지 않기 때문에 제너레이터 함수 형태로 만들지 않아도 된다.
+  try {
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
+  }
+}
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -39,6 +60,10 @@ export default handleActions(
       ...state,
       user: null,
       checkError: error,
+    }),
+    [LOGOUT]: (state) => ({
+      ...state,
+      user: null,
     }),
   },
   initialState,
