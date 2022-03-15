41b32f60df83f5ea1cfb3ee023cf0208b574a926
leeuihyun <meta212121@naver.com>
2022-03-15 16:34:09 +0900

    Using localStorage and tempSetUser(action)-> maintain login

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -9,13 +9,27 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer, { rootSaga } from './modules';
 import createSagaMiddleware from 'redux-saga';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return;
+    store.dispatch(tempSetUser(JSON.parse(user)));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage index error');
+  }
+}
 sagaMiddleware.run(rootSaga);
+loadUser();
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
