e00569da24ec337942b58328319c2daa47de4832
leeuihyun <meta212121@naver.com>
2022-03-15 16:16:12 +0900

    LoginForm maintain user state(login)

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -50,6 +50,11 @@ function LoginForm({ history }) {
   useEffect(() => {
     if (user) {
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [history, user]);
   return (
