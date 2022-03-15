2755142bf4d535bb5a796919fb06127701fb3a86
leeuihyun <meta212121@naver.com>
2022-03-15 15:57:44 +0900

    HeaderContainer

---
+++ blog/blog-frontend/src/containers/common/HeaderContainer.js
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import Header from '../../components/common/Header';
+
+const HeaderContainer = () => {
+  const { user } = useSelector(({ user }) => ({
+    user: user.user,
+  }));
+  return <Header user={user}></Header>;
+};
+
+export default HeaderContainer;
