2153d217e6a793cc563e2c8ebd6f3731dff13dfa
leeuihyun <meta212121@naver.com>
2022-03-17 16:39:47 +0900

    rootReducer,rootSaga (index.js module)

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -3,7 +3,7 @@ import auth, { authSaga } from './auth';
 import loading from './loading';
 import { all } from 'redux-saga/effects';
 import user, { userSaga } from './user';
-import write from './write';
+import write, { writeSaga } from './write';
 
 const rootReducer = combineReducers({
   auth,
@@ -13,6 +13,6 @@ const rootReducer = combineReducers({
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga()]);
 }
 export default rootReducer;
