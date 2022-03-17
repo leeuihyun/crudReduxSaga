c68c904950873aac0d44863a5f6a5d220033d9ab
leeuihyun <meta212121@naver.com>
2022-03-17 20:15:15 +0900

    PostPage Page

--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,11 +1,11 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
-import PostViewer from '../components/post/PostViewer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 function PostPage() {
   return (
     <>
       <HeaderContainer />
-      <PostViewer />
+      <PostViewerContainer />
     </>
   );
 }
