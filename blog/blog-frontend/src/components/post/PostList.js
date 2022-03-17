b343a63f32782c753ccfe691512537b244bb1d0a
leeuihyun <meta212121@naver.com>
2022-03-18 01:30:43 +0900

    PostList component

---
+++ blog/blog-frontend/src/components/post/PostList.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from '../common/Responsive';
+import Button from '../common/Button';
+import palette from '../../lib/styles/palette';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
+
+const PostListBlock = styled(Responsive)`
+  margin-top: 3rem;
+`;
+
+const WritePostButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 3rem;
+`;
+
+const PostItemBlock = styled.div`
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  &:first-child {
+    padding-top: 0;
+  }
+  & + & {
+    border-top: 1px solid ${palette.gray[2]};
+  }
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 0;
+    margin-top: 0;
+    &:hover {
+      color: ${palette.gray[6]};
+    }
+  }
+  p {
+    margin-top: 2rem;
+  }
+`;
+
+const PostItem = () => {
+  return (
+    <PostItemBlock>
+      <h2>제목</h2>
+      <SubInfo username="username" publishedDate={new Date()} />
+      <Tags tags={['tag1', 'tag2']} />
+      <p>포스트 내용의 일부..</p>
+    </PostItemBlock>
+  );
+};
+
+function PostList() {
+  return (
+    <PostListBlock>
+      <WritePostButtonWrapper>
+        <Button cyan="true" to="/write">
+          새 글 작성하기
+        </Button>
+      </WritePostButtonWrapper>
+      <div>
+        <PostItem />
+        <PostItem />
+        <PostItem />
+      </div>
+    </PostListBlock>
+  );
+}
+
+export default PostList;
